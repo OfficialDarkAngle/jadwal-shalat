840d2ea3d0c0fb12b2a2d14adda6f871a8f71471
Acep Triana <49948930+aceptriana@users.noreply.github.com>
2019-05-30 04:51:24 +0700

    Add files via upload

---
+++ shalat.sh
@@ -0,0 +1,13 @@
+figlet -f slant "waktu shalat"
+echo "* Powerd By Acep X-Code" | lolcat
+echo "* Manusia Biasa Team " | lolcat
+echo "* github.com/aceptriana" | lolcat
+sleep 1
+echo "sedang menginstall..."
+sleep 2
+git clone https://github.com/karjok/sholat
+clear
+cd sholat
+pip install -r modul.txt
+clear
+python sholat.py
